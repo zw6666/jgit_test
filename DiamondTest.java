ce2af3405f42f4d634754012c27c124d0c357d2b
Zhao <2714709357@qq.com>
2020-05-08 23:39:10 +0800

    2020-05-08 23:39:10:zw6666

---
+++ DiamondTest.java
@@ -0,0 +1,19 @@
+package com.example.refactor.test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class DiamondTest {
+
+    void print(){
+        List<String> list = new ArrayList<>();
+        HashMap<String,String> map2 = new HashMap<>();
+        HashMap<String,String>map = new HashMap<>();
+        HashMap<String,AstTest>map1 = new HashMap<>();
+        List<String>list1 = new ArrayList<>();
+        List list2 = new ArrayList(10);
+        HashMap map3 = new HashMap(5);
+    }
+}
+
